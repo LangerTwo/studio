063cd1d2fe7b36eb09d3f9b05d5de12faac81ec0
Alan Rangel <131817892+LangerTwo@users.noreply.github.com>
2023-09-22 13:44:18 -0500

    Add files via upload

---
+++ Practica Js/valores.js
@@ -0,0 +1,17 @@
+//valores primitivos
+
+40 // numero
+
+"palabas" // string
+
+true
+false // booleanos
+
+null
+undefined // vacio
+
+//no primitivos
+
+[1,2,3]//objeto
+
+{nombre: "alan"}
---
+++ Practica Js/variables.js
@@ -0,0 +1,19 @@
+var nombre = "Alan"; //variable
+
+// declarar
+var edad;
+// inicializar
+edad = 28;
+
+edad
+
+// array
+ var elementos = ["Computadora", "Celular"];
+
+ // objeto
+ var persona ={
+    nombre: "Alan",
+    edad: 30
+ };
+
+ persona
